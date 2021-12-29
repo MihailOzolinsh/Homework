ed367d79deb13bc0b4a37c48e84560bace17a71d
NeoKeif <misha.ozolinsh@gmail.com>
2021-12-29 13:37:25 +0100

    homework-2 commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,52 +1,48 @@
 'use strict';
-const products = [
- {id: 1, title: 'Notebook', price: 1000},
- {id: 2, title: 'Mouse', price: 100},
- {id: 3, title: 'Keyboard', price: 250},
- {id: 4, title: 'Gamepad', price: 150},
-];
-products.forEach(product => {
-   const productRender = '<div class="product-item"><h3>' + product.title + '</h3><p>' + product.price + '</p></div>';
-   document.querySelector('.products').insertAdjacentHTML('beforeend', productRender);
-})
-/* 
- Ответ на вопрос почему добавляются ',': 
-  Они добавляются так как это массив map а у массива добавляется такое значение после каждого ключа.
- Решение этого вопроса:
-  Его можно соединить с помощью join
-   Было: const productList = list.map(item => renderProduct(item.title, item.price));
-   Стало: const productList = list.map(item => renderProduct(item.title, item.price)).join('');
-*/
 
-/* 
- Как уменьшить код:
- Было: 
- 'use strict';
- const products = [
-  {id: 1, title: 'Notebook', price: 1000},
-  {id: 2, title: 'Mouse', price: 100},
-  {id: 3, title: 'Keyboard', price: 250},
-  {id: 4, title: 'Gamepad', price: 150},
- ];
- const renderProduct = (title, price) => {
-  return '<div class="product-item"><h3>' + title + '</h3><p>' + price + '</p></div>'
- };
- const renderCatalog = (list) => {
-  const productList = list.map(item => renderProduct(item.title, item.price)).join('');
-  document.querySelector('.products').innerHTML = productList;
- };
- renderCatalog(products);
- ----------------------------------------------------------------
- Стало:
- 'use strict';
- const products = [
-  {id: 1, title: 'Notebook', price: 1000},
-  {id: 2, title: 'Mouse', price: 100},
-  {id: 3, title: 'Keyboard', price: 250},
-  {id: 4, title: 'Gamepad', price: 150},
- ];
- products.forEach(product => {
-   const productRender = '<div class="product-item"><h3>' + product.title + '</h3><p>' + product.price + '</p></div>';
-   document.querySelector('.products').insertAdjacentHTML('beforeend', productRender);
-   })
-*/ 
+class GoodsList {
+   constructor() {
+      this.burgers = [];
+      this.totalGood = [];
+      this.totalPriceOfMenu = 0;
+      this.renderGoods();
+      this.calPrice();
+      this.render();
+   }
+   renderGoods() {
+      this.burgers = [
+         {id: 1, title: 'Burger', size: 'Small', price: 50, calories: 20,},
+         {id: 2, title: 'Burger', size: 'Big', price: 100, calories: 40,},
+         {id: 3, title: 'Sauce', price: 10, calories: 20,},
+         {id: 4, title: 'Salad', price: 20, calories: 5,},
+         {id: 5, title: 'Potato', price: 15, calories: 10,},
+      ];
+   }
+   calPrice() {
+      this.burgers.forEach(burger => {
+         this.totalPriceOfMenu += burger.price;
+      })
+      document.querySelector('.total-price').insertAdjacentHTML('beforeend', '<p>' + 'Total Price: ' + this.totalPriceOfMenu + '</p>');
+   }
+   render() {
+      this.burgers.forEach(burger => {
+         const Product = new Element(burger)
+         this.totalGood.push(Product);
+         document.querySelector('.products').insertAdjacentHTML('beforeend', Product.HTMLStringBurger());
+      })
+   }
+}
+
+class Element {
+   constructor(product) {
+      this.id = product.id;
+      this.title = product.title;
+      this.price = product.price;
+      this.calories = product.calories;
+   }
+   HTMLStringBurger() {
+      return '</gr><br><div><p>' + this.title + '</p><p>' + this.price + '</p><p>' + this.calories + '</p></div><br><hr>'
+   }
+}
+
+const good = new GoodsList();
